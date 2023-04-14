a755c8d123475f132b0de0bd12a00ecdb9d76ab7
matth747 <109696245+matth747@users.noreply.github.com>
2023-04-14 16:30:33 -0500

    added new signup page

--- public/js/signUserUp.js
+++ public/js/signUserUp.js
@@ -1,8 +1,8 @@
 $("#button").click(async function(event){
     event.preventDefault()
-    const name = $("#name").val();
-    const email = $("#email").val();
-    const password = $("#password").val();
+    const name = $("#name1").val();
+    const email = $("#email1").val();
+    const password = $("#password1").val();
 
     if (name && email && password) {
       // Send a POST request to the API endpoint
