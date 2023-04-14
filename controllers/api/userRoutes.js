f38cbd1e240e636c18d066680eb7317dde8cc714
matth747 <109696245+matth747@users.noreply.github.com>
2023-04-14 14:35:52 -0500

    fixing signup page

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const {foundEmail} = require('../../utils/newRegSendGrid');
 
-router.post('/singUp', async (req,res) => {
+router.post('/signUp', async (req,res) => {
     try {
         console.log(req.body)
         const userData = await User.create({
